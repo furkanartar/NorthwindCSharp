9f7bd8a11cf3a4ecf32f66b18dabb963ef07c9fc
ASosyaL <asosyalgenctvtr@gmail.com>
2021-02-21 15:26:28 +0300

    21 February

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
