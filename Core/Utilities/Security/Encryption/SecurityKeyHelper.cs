771c79ac87c150369d1a7edf853df6a0c853baaf
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2021-03-06 19:12:03 +0300

    For JWT added new files.

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey)//api'nin securitykey'ini symmetric key'e çeviriyor. asp için gerekli.
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
